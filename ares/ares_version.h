06f341e2efc7cec719248797e5557b3fb6cc90a9
bagder <bagder>
2009-11-30 22:39:04 +0000

    start working on 1.7.1

--- ares/ares_version.h
+++ ares/ares_version.h
@@ -1,15 +1,15 @@
-/* $Id: ares_version.h,v 1.19 2009-11-23 12:03:33 yangtse Exp $ */
+/* $Id: ares_version.h,v 1.20 2009-11-30 22:39:04 bagder Exp $ */
 
 #ifndef ARES__VERSION_H
 #define ARES__VERSION_H
 
 #define ARES_VERSION_MAJOR 1
 #define ARES_VERSION_MINOR 7
-#define ARES_VERSION_PATCH 0
+#define ARES_VERSION_PATCH 1
 #define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
                        (ARES_VERSION_MINOR<<8)|\
                        (ARES_VERSION_PATCH))
-#define ARES_VERSION_STR "1.7.0-CVS"
+#define ARES_VERSION_STR "1.7.1-CVS"
 
 #if (ARES_VERSION >= 0x010700)
 #  define CARES_HAVE_ARES_LIBRARY_INIT 1
