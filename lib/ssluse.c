45c85b0dd5075d7f6eddb57f778cede6d03f111b
bagder <bagder>
2009-12-10 20:19:56 +0000

    minor indent change

--- lib/ssluse.c
+++ lib/ssluse.c
@@ -18,7 +18,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: ssluse.c,v 1.247 2009-11-18 10:33:55 yangtse Exp $
+ * $Id: ssluse.c,v 1.248 2009-12-10 20:19:56 bagder Exp $
  ***************************************************************************/
 
 /*
@@ -1365,7 +1365,7 @@ static void ssl_tls_trace(int direction, int ssl_ver, int content_type,
   int  ver, msg_type, txt_len;
 
   if(!conn || !conn->data || !conn->data->set.fdebug ||
-      (direction != 0 && direction != 1))
+     (direction != 0 && direction != 1))
     return;
 
   data = conn->data;
@@ -1470,7 +1470,7 @@ ossl_connect_step1(struct connectdata *conn,
       infof(data, "SSL: couldn't set callback!\n");
     }
     else if(!SSL_CTX_ctrl(connssl->ctx, SSL_CTRL_SET_MSG_CALLBACK_ARG, 0,
-                           conn)) {
+                          conn)) {
       infof(data, "SSL: couldn't set callback argument!\n");
     }
   }
