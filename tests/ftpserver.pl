59d18080e05bdf73f109ca0da73f3d7ab7e3bc87
yangtse <yangtse>
2009-12-21 14:33:01 +0000

    Reinstate hi resolution time logging since this is not what is now adding 90 extra seconds to the total time some autobuilds need to make a full test-run.

--- tests/ftpserver.pl
+++ tests/ftpserver.pl
@@ -19,7 +19,7 @@
 # This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
 # KIND, either express or implied.
 #
-# $Id: ftpserver.pl,v 1.112 2009-12-21 08:33:47 bagder Exp $
+# $Id: ftpserver.pl,v 1.113 2009-12-21 14:33:01 yangtse Exp $
 ###########################################################################
 
 # This is a server designed for the curl test suite.
@@ -43,11 +43,12 @@ use IPC::Open2;
 require "getpart.pm";
 require "ftp.pm";
 
-#BEGIN {
-#    if($] > 5.006) {
-#        use Time::HiRes qw( gettimeofday );
-#    }
-#}
+BEGIN {
+    if($] >= 5.007003) {
+        require Time::HiRes;
+        import  Time::HiRes qw( gettimeofday );
+    }
+}
 
 #**********************************************************************
 # global vars...
@@ -127,18 +128,18 @@ sub getlogfilename {
 #
 sub logmsg {
     my $now;
-#   if($] > 5.006) {
-#       my ($seconds, $usec) = gettimeofday();
-#       my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
-#           localtime($seconds);
-#       $now = sprintf("%02d:%02d:%02d.%06d ", $hour, $min, $sec, $usec);
-#   }
-#   else {
+    if($] >= 5.007003) {
+        my ($seconds, $usec) = gettimeofday();
+        my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+            localtime($seconds);
+        $now = sprintf("%02d:%02d:%02d.%06d ", $hour, $min, $sec, $usec);
+    }
+    else {
         my $seconds = time();
         my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
             localtime($seconds);
         $now = sprintf("%02d:%02d:%02d ", $hour, $min, $sec);
-#   }
+    }
     if(open(LOGFILEFH, ">>$logfilename")) {
         print LOGFILEFH $now;
         print LOGFILEFH @_;
