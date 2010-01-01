77e8b4dd0df9f1fd7e301a41bc4d7456aa5bce01
bagder <bagder>
2010-01-01 14:52:50 +0000

    update copyright year since we are in 2010 now

--- lib/urldata.h
+++ lib/urldata.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2009, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2010, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: urldata.h,v 1.429 2010-01-01 14:44:45 bagder Exp $
+ * $Id: urldata.h,v 1.430 2010-01-01 14:52:52 bagder Exp $
  ***************************************************************************/
 
 /* This file is for lib internal stuff */
