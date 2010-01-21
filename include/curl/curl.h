d51acb4e5fab6b854c231c65eea4be047b048244
bagder <bagder>
2010-01-21 14:05:06 +0000

    remove typedef we ended up not using

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -20,7 +20,7 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- * $Id: curl.h,v 1.404 2010-01-21 13:58:30 bagder Exp $
+ * $Id: curl.h,v 1.405 2010-01-21 14:05:06 bagder Exp $
  ***************************************************************************/
 
 /*
@@ -196,12 +196,6 @@ typedef size_t (*curl_write_callback)(char *buffer,
                                       size_t size,
                                       size_t nitems,
                                       void *outstream);
-
-typedef size_t (*curl_rtp_write_callback)(char *buffer,
-                                      size_t size,
-                                      size_t nitems,
-                                      void *outstream,
-                                      int channel);
 
 /* These are the return codes for the seek callbacks */
 #define CURL_SEEKFUNC_OK       0
