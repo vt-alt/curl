a3bb81f5b3fe5458c1649f463f0973bb514928cd
yangtse <yangtse>
2010-01-21 14:25:41 +0000

    improve displaylogcontent() sub fixing a warning

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -19,7 +19,7 @@
 # This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
 # KIND, either express or implied.
 #
-# $Id: runtests.pl,v 1.379 2010-01-20 21:16:32 yangtse Exp $
+# $Id: runtests.pl,v 1.380 2010-01-21 14:25:41 yangtse Exp $
 ###########################################################################
 
 # Experimental hooks are available to run tests remotely on machines that
@@ -3683,10 +3683,17 @@ sub displaylogcontent {
                 $truncate = $linecount > 1000;
             }
         }
-        if (@tail) {
-            logmsg "=== File too long: lines here were removed\n";
-            # This won't work properly if time stamps are enabled in logmsg
-            logmsg join('',@tail[$#tail-200..$#tail]);
+        if(@tail) {
+            my $tailshow = 200;
+            my $tailskip = 0;
+            my $tailtotal = scalar @tail;
+            if($tailtotal > $tailshow) {
+                $tailskip = $tailtotal - $tailshow;
+                logmsg "=== File too long: $tailskip lines omitted here\n";
+            }
+            for($tailskip .. $tailtotal-1) {
+                logmsg "$tail[$_]";
+            }
         }
         close(SINGLE);
     }
